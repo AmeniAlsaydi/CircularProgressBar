04d45c638c37a1ccaef4a23cbfe3252dd4e3dd64
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-06-08 02:08:16 -0400

    tested progress bar with simulated loading

--- Progress/ProgressBarView.swift
+++ Progress/ProgressBarView.swift
@@ -12,9 +12,13 @@ class ProgressBarView: UIView {
     
     private var backgroundLayer: CAShapeLayer!
     private var foregroundLayer: CAShapeLayer!
-    private var textLayer: CAShapeLayer!
+    private var textLayer: CATextLayer!
     
-
+    public var progress: CGFloat = 0 { // will be progress bar stroke end value [ 0 - 1 ]
+        didSet {
+            didUpdateProgress()
+        }
+    }
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -26,26 +30,72 @@ class ProgressBarView: UIView {
         
         let lineWidth = 0.1 * min(width, height)
         
-        let center = CGPoint(x: width/2, y: height/2)
-        let radius = (min(width,height) - lineWidth) / 2
+        backgroundLayer = createCircularLayer(strokeColor: UIColor.lightGray.cgColor, fillColor: UIColor.clear.cgColor, lineWidth: lineWidth)
         
+        // aka progress layer
+        foregroundLayer = createCircularLayer(strokeColor: UIColor.red.cgColor, fillColor: UIColor.clear.cgColor, lineWidth: lineWidth)
         
-        let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat.pi, clockwise: true)
+        textLayer = createTextLayer(textColor: UIColor.white)
         
-        backgroundLayer = CAShapeLayer()
-        
-        backgroundLayer.path = circularPath.cgPath
-        
-        backgroundLayer.strokeColor = UIColor.lightGray.cgColor
-        backgroundLayer.fillColor = UIColor.clear.cgColor
-        backgroundLayer.lineWidth = lineWidth
-        backgroundLayer.lineCap = .round
+        //progress = 0
+        foregroundLayer.strokeEnd = 0
+        //foregroundLayer.strokeEnd = 0.3 // this is what i will use to display the progress of the application - incrementing or decrementing by 0.2
         
         layer.addSublayer(backgroundLayer)
-        
+        layer.addSublayer(foregroundLayer)
+        layer.addSublayer(textLayer)
         
         
     }
-   
-
+    
+    private func createCircularLayer(strokeColor: CGColor, fillColor: CGColor, lineWidth: CGFloat) -> CAShapeLayer {
+        
+        let startAngle = -CGFloat.pi / 2
+        let endAngle = 2 * CGFloat.pi + startAngle
+        
+        let width = frame.size.width
+        let height = frame.size.height
+        
+        let center = CGPoint(x: width / 2, y: height / 2)
+        let radius = (min(width, height) - lineWidth) / 2
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = strokeColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = fillColor
+        shapeLayer.lineCap = .round
+        
+        return shapeLayer
+    }
+    
+    private func createTextLayer(textColor: UIColor) -> CATextLayer {
+        
+        let width = frame.size.width
+        let height = frame.size.height
+        
+        let fontSize = min(width, height) / 4 - 5
+        let offset = min(width, height) * 0.1
+        
+        let layer = CATextLayer()
+        
+        layer.string = "\(Int(progress * 100))"
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.foregroundColor = textColor.cgColor
+        layer.fontSize = fontSize
+        layer.frame = CGRect(x: 0, y: (height - fontSize - offset) / 2, width: width, height: height)
+        layer.alignmentMode = .center
+        
+        return layer
+    }
+    
+    private func didUpdateProgress() {
+        textLayer?.string = "\(Int(progress * 100))" // ??
+        foregroundLayer?.strokeEnd = progress
+    }
+    
 }
--- Progress/ViewController.swift
+++ Progress/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressBar: ProgressBarView!
+    
+    var countFired: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self , action: #selector(handleTap)))
 
-        
-        
     }
     
-    
+    @objc public func handleTap() {
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            self.countFired += 1
+            
+            DispatchQueue.main.async {
+                self.progressBar.progress = min(CGFloat(0.03 * self.countFired), 1) // finds min between the progress value and 1 - this is to ensure that the progress bar will stop at 1
+                
+                
+                if self.progressBar.progress == 1 {
+                    timer.invalidate() // stop timer
+                }
+            }
+        }
+    }
 
 }
 
